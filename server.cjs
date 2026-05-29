f70ac328534c0c0a5c7dd33e8dc945f3d0b130fc
Cesar granjeno <cesar.granjeno@dtcco.io>
2026-05-28 18:10:43 -0600

    Add Route Parameters

--- server.cjs
+++ server.cjs
@@ -9,6 +9,25 @@ app.get("/about", (req, res) => {
   res.send("About page");
 });
 
+app.get("/products", (req, res) => {
+  res.json([
+    { id: 1, name: "Laptop", price: 1299 },
+    { id: 2, name: "Mouse", price: 50 },
+  ]);
+});
+
+app.get("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const products = [
+    { id: 1, name: "Laptop", price: 1299 },
+    { id: 2, name: "Mouse", price: 50 },
+  ];
+
+  const requestedProduct = products.find((product) => product.id === id);
+  res.json(requestedProduct);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
